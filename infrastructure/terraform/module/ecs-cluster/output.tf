608d51ece703ea76029be5c8857cf13b73e55d2e
monaboiste <monikaszumna4@gmail.com>
2022-08-11 17:02:59 +0200

    Temporary remove internal ALB

--- infrastructure/terraform/module/ecs-cluster/output.tf
+++ infrastructure/terraform/module/ecs-cluster/output.tf
@@ -14,14 +14,14 @@ output "alb_ext_dns_name" {
   value = aws_alb.alb_ext.dns_name
 }
 
-output "alb_int_dns_name" {
-  value = aws_alb.alb_int.dns_name
-}
+#output "alb_int_dns_name" {
+#  value = aws_alb.alb_int.dns_name
+#}
 
 output "alb_ext_default_security_group_ids" {
   value = aws_alb.alb_ext.security_groups
 }
 
-output "alb_int_default_security_group_ids" {
-  value = aws_alb.alb_int.security_groups
-}
+#output "alb_int_default_security_group_ids" {
+#  value = aws_alb.alb_int.security_groups
+#}
